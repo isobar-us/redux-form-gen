f7f51868425f1d49edebb91ad9badbe6b4efadc2
Brandon Vandegrift <bmv437@gmail.com>
2018-01-16 16:20:12 -0500

    upgrade jest and enzyme. fix examples package names

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  coverageReporters: ['html'],
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|bmp|ico|yml)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '^.+\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  },
+  modulePaths: ['src'],
+  testRegex: '(/__tests__/.*\\.spec.js)$',
+  verbose: true,
+  setupFiles: ['./__tests__/setup', 'raf/polyfill']
+};
