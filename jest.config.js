4b9044aeb6e66801dfb2ca93a6bec0f1685df3c3
Brandon Vandegrift <bmv437@gmail.com>
2018-01-22 09:32:54 -0500

    more flowtype coverage exemptions

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverage: true,
   coverageReporters: ['html', 'lcov'],
-  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/**/*.types.{js,jsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.types.{js,jsx}',
+    '!src/types.js'
+  ],
   moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|bmp|ico|yml)$':
